638ba329aea24dc8049238befacdcbc992179bed
TJ Draper <tj@buzzingpixel.com>
2019-01-15 22:02:41 -0600

    Add logout action

---
+++ src/app/http/actions/LogOutAction.php
@@ -0,0 +1,53 @@
+<?php
+declare(strict_types=1);
+
+namespace src\app\http\actions;
+
+use Psr\Http\Message\ResponseInterface;
+use corbomite\http\exceptions\Http404Exception;
+use corbomite\user\interfaces\UserApiInterface;
+use corbomite\flashdata\interfaces\FlashDataApiInterface;
+
+class LogOutAction
+{
+    private $userApi;
+    private $response;
+    private $flashDataApi;
+
+    public function __construct(
+        UserApiInterface $userApi,
+        ResponseInterface $response,
+        FlashDataApiInterface $flashDataApi
+    ) {
+        $this->userApi = $userApi;
+        $this->response = $response;
+        $this->flashDataApi = $flashDataApi;
+    }
+
+    /**
+     * @throws Http404Exception
+     */
+    public function __invoke(): ResponseInterface
+    {
+        if (! $this->userApi->fetchCurrentUser()) {
+            throw new Http404Exception();
+        }
+
+        $this->userApi->logCurrentUserOut();
+
+        $flashDataModel =$this->flashDataApi->makeFlashDataModel([
+            'name' => 'LogOutAction'
+        ]);
+
+        $flashDataModel->dataItem('success', true);
+
+        /** @noinspection PhpUnhandledExceptionInspection */
+        $this->flashDataApi->setFlashData($flashDataModel);
+
+        $response = $this->response->withHeader('Location', '/');
+
+        $response = $response->withStatus(303);
+
+        return $response;
+    }
+}
--- src/config/http/actionParams.php
+++ src/config/http/actionParams.php
@@ -1,10 +1,12 @@
 <?php
 declare(strict_types=1);
 
+use src\app\http\actions\LogOutAction;
 use src\app\http\actions\ResetPasswordAction;
 use src\app\http\actions\SendPasswordResetEmailAction;
 
 return [
+    'logout' => ['class' => LogOutAction::class],
     'resetPassword' => ['class' => ResetPasswordAction::class],
     'sendPasswordResetEmail' => ['class' => SendPasswordResetEmailAction::class],
 ];
