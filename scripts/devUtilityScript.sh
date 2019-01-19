750692a4bca9e364e6d4c17de6ed12c437eab8eb
TJ Draper <tj@buzzingpixel.com>
2019-01-19 14:27:14 -0600

    Update for running on *nix systems

--- scripts/devUtilityScript.sh
+++ scripts/devUtilityScript.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
-COUNTER=1;
-
 while true; do
     rm -rf /app/cache/twig/*;
     chmod -R 0777 /app/cache;
     chmod -R 0777 /var/lib/mysql;
-    echo ${COUNTER} > /app/dev/null/tmp;
+    chmod -R 0777 /app/public/cache;
     rsync -av /app/vendor/ /vendor-volume --delete
-    COUNTER=$((COUNTER+1));
     sleep 2;
 done
