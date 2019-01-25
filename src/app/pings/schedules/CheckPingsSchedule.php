68469d97219893e9a1bbba0632c6ca6b68194c79
TJ Draper <tj@buzzingpixel.com>
2019-01-24 19:11:08 -0600

    Add schedule for adding the collect pings to queue

--- src/app/pings/schedules/CheckPingsSchedule.php
+++ src/app/pings/schedules/CheckPingsSchedule.php
@@ -3,7 +3,10 @@ declare(strict_types=1);
 
 namespace src\app\pings\schedules;
 
+use src\app\pings\tasks\CheckPingTask;
 use corbomite\queue\interfaces\QueueApiInterface;
+use src\app\pings\tasks\CollectPingsForQueueTask;
+use corbomite\queue\exceptions\InvalidActionQueueBatchModel;
 
 class CheckPingsSchedule
 {
@@ -14,8 +17,30 @@ class CheckPingsSchedule
         $this->queueApi = $queueApi;
     }
 
+    /**
+     * @throws InvalidActionQueueBatchModel
+     */
     public function __invoke()
     {
-        // TODO: Implement __invoke() method.
+        $queryModel = $this->queueApi->makeQueryModel();
+        $queryModel->addWhere('name', CollectPingsForQueueTask::BATCH_NAME);
+        $queryModel->addWhere('is_finished', '0');
+        $queryModel->addWhere('name', CheckPingTask::BATCH_NAME, '=', true);
+        $queryModel->addWhere('is_finished', '0');
+        $existingBatchItem = $this->queueApi->fetchOneBatch($queryModel);
+
+        if ($existingBatchItem) {
+            return;
+        }
+
+        $item = $this->queueApi->makeActionQueueItemModel();
+        $item->class(CollectPingsForQueueTask::class);
+
+        $batch = $this->queueApi->makeActionQueueBatchModel();
+        $batch->name(CollectPingsForQueueTask::BATCH_NAME);
+        $batch->title(CollectPingsForQueueTask::BATCH_TITLE);
+        $batch->addItem($item);
+
+        $this->queueApi->addToQueue($batch);
     }
 }
--- src/config/scheduleConfig.php
+++ src/config/scheduleConfig.php
@@ -1,11 +1,16 @@
 <?php
 declare(strict_types=1);
 
+use src\app\pings\schedules\CheckPingsSchedule;
 use src\app\monitoredurls\schedules\CheckUrlsSchedule;
 
 return [
     [
         'class' => CheckUrlsSchedule::class,
+        'runEvery' => 'Always'
+    ],
+    [
+        'class' => CheckPingsSchedule::class,
         'runEvery' => 'Always'
     ],
 ];
