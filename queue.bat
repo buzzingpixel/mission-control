5897882094cffc36b71ac3ef7467d9fa436d20ef
TJ Draper <tj@buzzingpixel.com>
2019-03-06 15:01:37 -0600

    Update Docker config

--- queue.bat
+++ queue.bat
@@ -1,6 +1,4 @@
 @echo off
 
-type nul >> .env.override
-docker-compose up -d
-docker exec -it --user root php-mission-control bash -c "chmod +x /app/scripts/ensureComposerInstall.sh && /app/scripts/ensureComposerInstall.sh"
-docker exec -it --user root --workdir /app-www php-mission-control bash -c "chmod +x /app/scripts/queueRunner.sh && /app/scripts/queueRunner.sh"
+call up.bat
+docker exec -it --user root --workdir /app-www php-mission-control bash -c "chmod +x /app/scripts/dev/queueRunner.sh && /app/scripts/dev/queueRunner.sh"
--- run.bat
+++ run.bat
@@ -1,8 +1,6 @@
 @echo off
 
-type nul >> .env.override
 docker kill node-mission-control
-docker-compose up -d
-docker exec -it --user root php-mission-control bash -c "chmod +x /app/scripts/ensureComposerInstall.sh && /app/scripts/ensureComposerInstall.sh"
+call up.bat
 docker exec -it --user root node-mission-control bash -c "chmod +x /app/scripts/ensureNpmInstall.sh && /app/scripts/ensureNpmInstall.sh"
 docker exec -it --user root --workdir /app node-mission-control bash -c "npm run fab"
