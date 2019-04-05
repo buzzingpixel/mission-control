5580a8f4b2a7ddc824e2cac89bf7b99af8aa7917
TJ Draper <tj@buzzingpixel.com>
2019-04-05 12:02:06 -0500

    Update notifications classes to use more autowiring as able

--- src/config/di/notifications.php
+++ src/config/di/notifications.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use Psr\Container\ContainerInterface;
-use src\app\monitoredurls\MonitoredUrlsApi;
 use src\app\notifications\notificationadapters\SendEmailNotificationAdapter;
 use src\app\notifications\notificationadapters\SlackNotificationAdapter;
 use src\app\notifications\schedules\CheckPingsForNotificationsSchedule;
@@ -15,8 +14,6 @@ use src\app\notifications\tasks\CheckUrlForNotificationTask;
 use src\app\notifications\tasks\CollectPingsForNotificationQueueTask;
 use src\app\notifications\tasks\CollectRemindersForNotificationQueueTask;
 use src\app\notifications\tasks\CollectUrlsForNotificationQueueTask;
-use src\app\pings\PingApi;
-use src\app\reminders\ReminderApi;
 use src\app\support\extensions\GuzzleClientNoHttpErrors;
 use function DI\autowire;
 
@@ -30,24 +27,21 @@ return [
     CheckPingsForNotificationsSchedule::class => autowire(),
     CheckRemindersForNotificationsSchedule::class => autowire(),
     CheckUrlsForNotificationsSchedule::class => autowire(),
-    CheckPingForNotificationTask::class => static function (ContainerInterface $di) {
-        return new CheckPingForNotificationTask(
-            $di->get(PingApi::class),
-            $di->get('NotificationAdaptersArray')
-        );
-    },
-    CheckReminderForNotificationTask::class => static function (ContainerInterface $di) {
-        return new CheckReminderForNotificationTask(
-            $di->get(ReminderApi::class),
-            $di->get('NotificationAdaptersArray')
-        );
-    },
-    CheckUrlForNotificationTask::class => static function (ContainerInterface $di) {
-        return new CheckUrlForNotificationTask(
-            $di->get(MonitoredUrlsApi::class),
-            $di->get('NotificationAdaptersArray')
-        );
-    },
+    CheckPingForNotificationTask::class => autowire()
+        ->constructorParameter(
+            'sendNotificationAdapters',
+            DI\get('NotificationAdaptersArray')
+        ),
+    CheckReminderForNotificationTask::class => autowire()
+        ->constructorParameter(
+            'sendNotificationAdapters',
+            DI\get('NotificationAdaptersArray')
+        ),
+    CheckUrlForNotificationTask::class => autowire()
+        ->constructorParameter(
+            'sendNotificationAdapters',
+            DI\get('NotificationAdaptersArray')
+        ),
     CollectPingsForNotificationQueueTask::class => autowire(),
     CollectRemindersForNotificationQueueTask::class => autowire(),
     CollectUrlsForNotificationQueueTask::class => autowire(),
