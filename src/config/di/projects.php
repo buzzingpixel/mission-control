5a4823903fcd8bcafe213c9ce5f9dd43896e359d
TJ Draper <tj@buzzingpixel.com>
2019-04-05 12:07:43 -0500

    Use auto wired dependency injection for project classes

--- src/config/di/projects.php
+++ src/config/di/projects.php
@@ -2,11 +2,6 @@
 
 declare(strict_types=1);
 
-use Cocur\Slugify\Slugify;
-use corbomite\db\Factory as OrmFactory;
-use corbomite\db\services\BuildQueryService;
-use corbomite\events\EventDispatcher;
-use Psr\Container\ContainerInterface;
 use src\app\projects\interfaces\ProjectsApiInterface;
 use src\app\projects\ProjectsApi;
 use src\app\projects\services\ArchiveProjectService;
@@ -14,46 +9,14 @@ use src\app\projects\services\DeleteProjectService;
 use src\app\projects\services\FetchProjectsService;
 use src\app\projects\services\SaveProjectService;
 use src\app\projects\services\UnArchiveProjectService;
+use function DI\autowire;
 
 return [
-    ProjectsApi::class => static function (ContainerInterface $di) {
-        return new ProjectsApi($di);
-    },
-    ProjectsApiInterface::class => static function (ContainerInterface $di) {
-        return $di->get(ProjectsApi::class);
-    },
-    ArchiveProjectService::class => static function (ContainerInterface $di) {
-        return new ArchiveProjectService(
-            new OrmFactory(),
-            $di->get(BuildQueryService::class),
-            $di->get(EventDispatcher::class)
-        );
-    },
-    DeleteProjectService::class => static function (ContainerInterface $di) {
-        return new DeleteProjectService(
-            new OrmFactory(),
-            $di->get(BuildQueryService::class),
-            $di->get(EventDispatcher::class)
-        );
-    },
-    FetchProjectsService::class => static function (ContainerInterface $di) {
-        return new FetchProjectsService(
-            $di->get(BuildQueryService::class)
-        );
-    },
-    SaveProjectService::class => static function (ContainerInterface $di) {
-        return new SaveProjectService(
-            new Slugify(),
-            new OrmFactory(),
-            $di->get(BuildQueryService::class),
-            $di->get(EventDispatcher::class)
-        );
-    },
-    UnArchiveProjectService::class => static function (ContainerInterface $di) {
-        return new UnArchiveProjectService(
-            new OrmFactory(),
-            $di->get(BuildQueryService::class),
-            $di->get(EventDispatcher::class)
-        );
-    },
+    ProjectsApi::class => autowire(),
+    ProjectsApiInterface::class => autowire(ProjectsApi::class),
+    ArchiveProjectService::class => autowire(),
+    DeleteProjectService::class => autowire(),
+    FetchProjectsService::class => autowire(),
+    SaveProjectService::class => autowire(),
+    UnArchiveProjectService::class => autowire(),
 ];
