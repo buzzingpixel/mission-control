cb667472c7ca5b414d831c595adbe38d931e37b3
TJ Draper <tj@buzzingpixel.com>
2019-04-05 12:06:10 -0500

    Update Pipeline Classes to use auto wired dependency injection

--- src/config/di/pipelines.php
+++ src/config/di/pipelines.php
@@ -2,13 +2,6 @@
 
 declare(strict_types=1);
 
-use Atlas\Pdo\Connection;
-use Cocur\Slugify\Slugify;
-use corbomite\db\Factory as OrmFactory;
-use corbomite\db\services\BuildQueryService;
-use corbomite\events\EventDispatcher;
-use corbomite\queue\QueueApi;
-use Psr\Container\ContainerInterface;
 use src\app\pipelines\interfaces\PipelineApiInterface;
 use src\app\pipelines\listeners\SavePipelineJobListener;
 use src\app\pipelines\PipelineApi;
@@ -22,85 +15,24 @@ use src\app\pipelines\services\SavePipelineJobService;
 use src\app\pipelines\services\SavePipelineService;
 use src\app\pipelines\services\UnArchivePipelineService;
 use src\app\pipelines\tasks\RunJobItemTask;
-use src\app\pipelines\transformers\PipelineJobItemRecordModelTransformer;
-use src\app\pipelines\transformers\PipelineJobRecordModelTransformer;
-use src\app\pipelines\transformers\PipelineRecordModelTransformer;
+use function DI\autowire;
 
 return [
-    PipelineApi::class => static function (ContainerInterface $di) {
-        return new PipelineApi($di);
-    },
-    PipelineApiInterface::class => static function (ContainerInterface $di) {
-        return $di->get(PipelineApi::class);
-    },
-    ArchivePipelineService::class => static function (ContainerInterface $di) {
-        return new ArchivePipelineService(
-            new OrmFactory(),
-            $di->get(BuildQueryService::class),
-            $di->get(EventDispatcher::class)
-        );
-    },
-    DeletePipelineService::class => static function (ContainerInterface $di) {
-        return new DeletePipelineService(
-            new OrmFactory(),
-            $di->get(BuildQueryService::class),
-            $di->get(EventDispatcher::class)
-        );
-    },
-    FetchOnePipelineJobItemService::class => static function (ContainerInterface $di) {
-        return new FetchOnePipelineJobItemService(
-            $di->get(BuildQueryService::class),
-            $di->get(PipelineJobRecordModelTransformer::class),
-            $di->get(PipelineJobItemRecordModelTransformer::class)
-        );
-    },
-    FetchPipelineJobService::class => static function (ContainerInterface $di) {
-        return new FetchPipelineJobService(
-            $di->get(BuildQueryService::class),
-            $di->get(PipelineJobRecordModelTransformer::class)
-        );
-    },
-    FetchPipelineService::class => static function (ContainerInterface $di) {
-        return new FetchPipelineService(
-            $di->get(BuildQueryService::class),
-            $di->get(PipelineRecordModelTransformer::class)
-        );
-    },
-    InitJobFromPipelineModelService::class => static function (ContainerInterface $di) {
-        return new InitJobFromPipelineModelService(
-            $di->get(PipelineApi::class)
-        );
-    },
-    SavePipelineJobService::class => static function (ContainerInterface $di) {
-        return new SavePipelineJobService(
-            new OrmFactory(),
-            $di->get(BuildQueryService::class),
-            $di->get(EventDispatcher::class)
-        );
-    },
-    SavePipelineService::class => static function (ContainerInterface $di) {
-        return new SavePipelineService(
-            new Slugify(),
-            $di->get(Connection::class),
-            new OrmFactory(),
-            $di->get(BuildQueryService::class),
-            $di->get('UuidFactoryWithOrderedTimeCodec'),
-            $di->get(EventDispatcher::class)
-        );
-    },
-    UnArchivePipelineService::class => static function (ContainerInterface $di) {
-        return new UnArchivePipelineService(
-            new OrmFactory(),
-            $di->get(BuildQueryService::class),
-            $di->get(EventDispatcher::class)
-        );
-    },
-    SavePipelineJobListener::class => static function (ContainerInterface $di) {
-        return new SavePipelineJobListener(
-            $di->get(QueueApi::class)
-        );
-    },
-    RunJobItemTask::class => static function (ContainerInterface $di) {
-        return new RunJobItemTask($di->get(PipelineApi::class));
-    },
+    PipelineApi::class => autowire(),
+    PipelineApiInterface::class => autowire(PipelineApi::class),
+    ArchivePipelineService::class => autowire(),
+    DeletePipelineService::class => autowire(),
+    FetchOnePipelineJobItemService::class => autowire(),
+    FetchPipelineJobService::class => autowire(),
+    FetchPipelineService::class => autowire(),
+    InitJobFromPipelineModelService::class => autowire(),
+    SavePipelineJobService::class => autowire(),
+    SavePipelineService::class => autowire()
+        ->constructorParameter(
+            'uuidFactory',
+            DI\get('UuidFactoryWithOrderedTimeCodec')
+        ),
+    UnArchivePipelineService::class => autowire(),
+    SavePipelineJobListener::class => autowire(),
+    RunJobItemTask::class => autowire(),
 ];
