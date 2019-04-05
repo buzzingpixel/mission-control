2f876f8fc24e3660d4e0b7917dd6e578cee01374
TJ Draper <tj@buzzingpixel.com>
2019-04-05 11:03:27 -0500

    Use auto wiring on HTTP services

--- src/config/di/http/middlewares.php
+++ src/config/di/http/middlewares.php
@@ -2,14 +2,9 @@
 
 declare(strict_types=1);
 
-use Psr\Container\ContainerInterface;
-use src\app\http\controllers\RenderErrorPageController;
 use src\app\http\middlewares\ErrorPagesMiddleware;
+use function DI\autowire;
 
 return [
-    ErrorPagesMiddleware::class => static function (ContainerInterface $di) {
-        return new ErrorPagesMiddleware(
-            $di->get(RenderErrorPageController::class)
-        );
-    },
+    ErrorPagesMiddleware::class => autowire(),
 ];
--- src/config/di/http/services.php
+++ src/config/di/http/services.php
@@ -2,27 +2,11 @@
 
 declare(strict_types=1);
 
-use corbomite\twig\TwigEnvironment;
-use corbomite\user\UserApi;
-use Psr\Container\ContainerInterface;
 use src\app\http\services\RenderPipelineInnerComponents;
 use src\app\http\services\RequireLoginService;
-use src\app\pipelines\PipelineApi;
-use Zend\Diactoros\Response;
+use function DI\autowire;
 
 return [
-    RequireLoginService::class => static function (ContainerInterface $di) {
-        return new RequireLoginService(
-            $di->get(TwigEnvironment::class),
-            $di->get(UserApi::class),
-            new Response()
-        );
-    },
-    RenderPipelineInnerComponents::class => static function (ContainerInterface $di) {
-         return new RenderPipelineInnerComponents(
-             $di->get(UserApi::class),
-             $di->get(TwigEnvironment::class),
-             $di->get(PipelineApi::class)
-         );
-    },
+    RequireLoginService::class => autowire(),
+    RenderPipelineInnerComponents::class => autowire(),
 ];
