b96b98ea98e4cca1fd3866ab2d52d58943e4da15
TJ Draper <tj@buzzingpixel.com>
2019-04-05 11:29:50 -0500

    Update Monitored URLs classes to use auto wired dependency injection

--- src/config/di/foreign.php
+++ src/config/di/foreign.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 use buzzingpixel\corbomitemailer\EmailApi;
 use buzzingpixel\corbomitemailer\interfaces\EmailApiInterface;
+use corbomite\db\interfaces\BuildQueryInterface;
+use corbomite\db\services\BuildQueryService;
 use corbomite\flashdata\FlashDataApi;
 use corbomite\flashdata\interfaces\FlashDataApiInterface;
 use corbomite\http\interfaces\RequestHelperInterface;
@@ -19,30 +21,20 @@ use Psr\Http\Message\ResponseInterface;
 use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Output\OutputInterface;
 use Zend\Diactoros\ResponseFactory;
+use function DI\autowire;
 
 return [
-    DataStoreInterface::class => static function (ContainerInterface $di) {
-        return $di->get(DataStore::class);
-    },
-    EmailApiInterface::class => static function (ContainerInterface $di) {
-        return $di->get(EmailApi::class);
-    },
-    FlashDataApiInterface::class => static function (ContainerInterface $di) {
-        return $di->get(FlashDataApi::class);
-    },
-    OutputInterface::class => static function () {
-        return new ConsoleOutput();
-    },
-    QueueApiInterface::class => static function (ContainerInterface $di) {
-        return $di->get(QueueApi::class);
-    },
+    BuildQueryInterface::class => autowire(BuildQueryService::class),
+    DataStoreInterface::class => autowire(DataStore::class),
+    EmailApiInterface::class => autowire(EmailApi::class),
+    FlashDataApiInterface::class => autowire(FlashDataApi::class),
+    OutputInterface::class => autowire(ConsoleOutput::class),
+    QueueApiInterface::class => autowire(QueueApi::class),
     RequestHelperInterface::class => static function (ContainerInterface $di) {
         return $di->get(RequestHelper::class);
     },
     ResponseInterface::class => static function () {
         return (new ResponseFactory())->createResponse();
     },
-    UserApiInterface::class => static function (ContainerInterface $di) {
-        return $di->get(UserApi::class);
-    },
+    UserApiInterface::class => autowire(UserApi::class),
 ];
--- src/config/di/monitoredurls.php
+++ src/config/di/monitoredurls.php
@@ -2,14 +2,6 @@
 
 declare(strict_types=1);
 
-use buzzingpixel\corbomitemailer\EmailApi;
-use Cocur\Slugify\Slugify;
-use corbomite\db\Factory as OrmFactory;
-use corbomite\db\PDO;
-use corbomite\db\services\BuildQueryService;
-use corbomite\events\EventDispatcher;
-use corbomite\queue\QueueApi;
-use Psr\Container\ContainerInterface;
 use src\app\monitoredurls\interfaces\MonitoredUrlsApiInterface;
 use src\app\monitoredurls\listeners\MonitoredUrlDeleteListener;
 use src\app\monitoredurls\listeners\ProjectArchiveListener;
@@ -26,97 +18,23 @@ use src\app\monitoredurls\services\SaveMonitoredUrlService;
 use src\app\monitoredurls\services\UnArchiveMonitoredUrlService;
 use src\app\monitoredurls\tasks\CheckUrlTask;
 use src\app\monitoredurls\tasks\CollectUrlsForQueueTask;
-use src\app\support\extensions\GuzzleClientNoHttpErrors;
+use function DI\autowire;
 
 return [
-    MonitoredUrlsApi::class => static function (ContainerInterface $di) {
-        return new MonitoredUrlsApi($di);
-    },
-    MonitoredUrlsApiInterface::class => static function (ContainerInterface $di) {
-        return $di->get(MonitoredUrlsApi::class);
-    },
-    ArchiveMonitoredUrlService::class => static function (ContainerInterface $di) {
-        return new ArchiveMonitoredUrlService(
-            new OrmFactory(),
-            $di->get(BuildQueryService::class),
-            $di->get(EventDispatcher::class)
-        );
-    },
-    DeleteMonitoredUrlService::class => static function (ContainerInterface $di) {
-        return new DeleteMonitoredUrlService(
-            new OrmFactory(),
-            $di->get(BuildQueryService::class),
-            $di->get(EventDispatcher::class)
-        );
-    },
-    FetchIncidentsService::class => static function (ContainerInterface $di) {
-        return new FetchIncidentsService(
-            $di->get(BuildQueryService::class)
-        );
-    },
-    FetchMonitoredUrlsService::class => static function (ContainerInterface $di) {
-        return new FetchMonitoredUrlsService(
-            $di->get(BuildQueryService::class)
-        );
-    },
-    SaveIncidentService::class => static function (ContainerInterface $di) {
-        return new SaveIncidentService(
-            new OrmFactory(),
-            $di->get(BuildQueryService::class),
-            $di->get(EventDispatcher::class)
-        );
-    },
-    SaveMonitoredUrlService::class => static function (ContainerInterface $di) {
-        return new SaveMonitoredUrlService(
-            new Slugify(),
-            new OrmFactory(),
-            $di->get(BuildQueryService::class),
-            $di->get(EventDispatcher::class)
-        );
-    },
-    UnArchiveMonitoredUrlService::class => static function (ContainerInterface $di) {
-        return new UnArchiveMonitoredUrlService(
-            new OrmFactory(),
-            $di->get(BuildQueryService::class),
-            $di->get(EventDispatcher::class)
-        );
-    },
-    MonitoredUrlDeleteListener::class => static function (ContainerInterface $di) {
-        return new MonitoredUrlDeleteListener(
-            $di->get(PDO::class)
-        );
-    },
-    ProjectArchiveListener::class => static function (ContainerInterface $di) {
-        return new ProjectArchiveListener(
-            $di->get(MonitoredUrlsApi::class)
-        );
-    },
-    ProjectUnArchiveListener::class => static function (ContainerInterface $di) {
-        return new ProjectUnArchiveListener(
-            $di->get(MonitoredUrlsApi::class)
-        );
-    },
-    ProjectDeleteListener::class => static function (ContainerInterface $di) {
-        return new ProjectDeleteListener(
-            $di->get(MonitoredUrlsApi::class)
-        );
-    },
-    CheckUrlsSchedule::class => static function (ContainerInterface $di) {
-        return new CheckUrlsSchedule(
-            $di->get(QueueApi::class)
-        );
-    },
-    CollectUrlsForQueueTask::class => static function (ContainerInterface $di) {
-        return new CollectUrlsForQueueTask(
-            $di->get(QueueApi::class),
-            $di->get(MonitoredUrlsApi::class)
-        );
-    },
-    CheckUrlTask::class => static function (ContainerInterface $di) {
-        return new CheckUrlTask(
-            $di->get(EmailApi::class),
-            new GuzzleClientNoHttpErrors(),
-            $di->get(MonitoredUrlsApi::class)
-        );
-    },
+    MonitoredUrlsApi::class => autowire(),
+    MonitoredUrlsApiInterface::class => autowire(MonitoredUrlsApi::class),
+    ArchiveMonitoredUrlService::class => autowire(),
+    DeleteMonitoredUrlService::class => autowire(),
+    FetchIncidentsService::class => autowire(),
+    FetchMonitoredUrlsService::class => autowire(),
+    SaveIncidentService::class => autowire(),
+    SaveMonitoredUrlService::class => autowire(),
+    UnArchiveMonitoredUrlService::class => autowire(),
+    MonitoredUrlDeleteListener::class => autowire(),
+    ProjectArchiveListener::class => autowire(),
+    ProjectUnArchiveListener::class => autowire(),
+    ProjectDeleteListener::class => autowire(),
+    CheckUrlsSchedule::class => autowire(),
+    CollectUrlsForQueueTask::class => autowire(),
+    CheckUrlTask::class => autowire(),
 ];
