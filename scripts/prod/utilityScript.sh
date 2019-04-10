3e34f7d0ed9aac8aa724df8358364919eb28b2ee
TJ Draper <tj@buzzingpixel.com>
2019-04-10 16:33:53 -0500

    Add docker configs for production

---
+++ scripts/prod/utilityScript.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [[ ! -f /db-volume/ib_buffer_pool ]]; then
+    chmod -R 0777 /root/db-starter;
+    cp -R /root/db-starter/* /db-volume/;
+    chmod -R 0777 /db-volume;
+fi
+
+while true; do
+    chmod -R 0777 /cache-volume;
+    chmod -R 0777 /log-volume;
+    sleep 2;
+done
