7834bc309545958fcf9c67633ec3264a94c1f46a
TJ Draper <tj@buzzingpixel.com>
2019-04-12 11:28:14 -0500

    Make prod docker schedule runner non-interactive

--- scripts/prod/scheduleRunner.sh
+++ scripts/prod/scheduleRunner.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker exec -it --user root --workdir /app php-mission-control bash -c "php /app/app schedule/run"
+docker exec --user root --workdir /app php-mission-control bash -c "php /app/app schedule/run"
