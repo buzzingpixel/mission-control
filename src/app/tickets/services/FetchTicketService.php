012310e0a9854020eb7fd31353665a5d1dd772b5
TJ Draper <tj@buzzingpixel.com>
2019-07-27 15:07:03 -0500

    Build out initial method for fetching tickets

--- src/app/tickets/services/FetchTicketService.php
+++ src/app/tickets/services/FetchTicketService.php
@@ -4,14 +4,99 @@ declare(strict_types=1);
 
 namespace src\app\tickets\services;
 
+use corbomite\db\interfaces\BuildQueryInterface;
 use corbomite\db\interfaces\QueryModelInterface;
-use function dd;
+use corbomite\user\interfaces\UserApiInterface;
+use DateTimeImmutable;
+use DateTimeZone;
+use src\app\data\Ticket\Ticket;
+use src\app\data\Ticket\TicketRecord;
+use src\app\tickets\interfaces\TicketModelContract;
+use src\app\tickets\models\TicketModel;
+use function array_values;
 
 class FetchTicketService
 {
-    public function fetch(QueryModelInterface $params) : void
+    /** @var BuildQueryInterface */
+    private $buildQuery;
+    /** @var UserApiInterface */
+    private $userApi;
+
+    public function __construct(
+        BuildQueryInterface $buildQuery,
+        UserApiInterface $userApi
+    ) {
+        $this->buildQuery = $buildQuery;
+        $this->userApi    = $userApi;
+    }
+
+    /**
+     * @return TicketModelContract[]
+     */
+    public function fetch(QueryModelInterface $params) : array
     {
-        // TODO: Implement method
-        dd('TODO: Implement ' . __METHOD__ . '()');
+        $results = $this->fetchResults($params);
+
+        $models = [];
+
+        $userIds = [];
+
+        foreach ($results as $record) {
+            $userIds[$record->created_by_user_guid] = $record->created_by_user_guid;
+
+            $userIds[$record->assigned_to_user_guid] = $record->assigned_to_user_guid;
+        }
+
+        $userQueryParams = $this->userApi->makeQueryModel();
+
+        $userQueryParams->addWhere('guid', array_values($userIds));
+
+        $users = [];
+
+        foreach ($this->userApi->fetchAll($userQueryParams) as $userModel) {
+            $users[$userModel->getGuidAsBytes()] = $userModel;
+        }
+
+        foreach ($results as $record) {
+            $model = new TicketModel();
+
+            $model->setGuidAsBytes($record->guid);
+
+            $model->createdByUser($users[$record->created_by_user_guid] ?? null);
+
+            $model->assignedToUser($users[$record->assigned_to_user_guid] ?? null);
+
+            $model->title($record->title);
+
+            $model->content($record->content);
+
+            $model->status($record->status);
+
+            if ($record->added_at_utc) {
+                /** @noinspection PhpUnhandledExceptionInspection */
+                $model->addedAt(new DateTimeImmutable($record->added_at_utc, new DateTimeZone('UTC')));
+            }
+
+            if ($record->resolved_at_utc) {
+                /** @noinspection PhpUnhandledExceptionInspection */
+                $model->addedAt(new DateTimeImmutable($record->resolved_at_utc, new DateTimeZone('UTC')));
+            }
+
+            // TODO: Thread Items
+
+            // TODO: Watchers
+
+            $models[] = $model;
+        }
+
+        return $models;
+    }
+
+    /**
+     * @return TicketRecord[]
+     */
+    private function fetchResults(QueryModelInterface $params) : array
+    {
+        return $this->buildQuery->build(Ticket::class, $params)->fetchRecords();
     }
 }
