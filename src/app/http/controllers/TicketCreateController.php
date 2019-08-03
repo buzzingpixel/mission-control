47bbac1cd7d7d9c8d44352123d116e8fbbe4845c
TJ Draper <tj@buzzingpixel.com>
2019-08-03 10:32:56 -0400

    Create page to create a new ticket

--- src/app/http/controllers/TicketCreateController.php
+++ src/app/http/controllers/TicketCreateController.php
@@ -4,11 +4,118 @@ declare(strict_types=1);
 
 namespace src\app\http\controllers;
 
+use corbomite\twig\TwigEnvironment;
+use corbomite\user\interfaces\UserApiInterface;
+use LogicException;
+use Psr\Http\Message\ResponseInterface;
+use src\app\http\services\RequireLoginService;
+use src\app\users\AdditionalUserActionsService;
+use Throwable;
+
 class TicketCreateController
 {
-    public function __invoke() : void
+    /** @var RequireLoginService */
+    private $requireLogin;
+    /** @var UserApiInterface */
+    private $userApi;
+    /** @var ResponseInterface */
+    private $response;
+    /** @var TwigEnvironment */
+    private $twigEnvironment;
+    /** @var AdditionalUserActionsService */
+    private $additionalUserActions;
+
+    public function __construct(
+        RequireLoginService $requireLogin,
+        UserApiInterface $userApi,
+        ResponseInterface $response,
+        TwigEnvironment $twigEnvironment,
+        AdditionalUserActionsService $additionalUserActions
+    ) {
+        $this->requireLogin          = $requireLogin;
+        $this->userApi               = $userApi;
+        $this->response              = $response;
+        $this->twigEnvironment       = $twigEnvironment;
+        $this->additionalUserActions = $additionalUserActions;
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function __invoke() : ResponseInterface
     {
-        // TODO: Implement method
-        dd('TODO: Implement ' . __METHOD__ . '()');
+        $requireLogin = $this->requireLogin->requireLogin();
+
+        if ($requireLogin) {
+            return $requireLogin;
+        }
+
+        $user = $this->userApi->fetchCurrentUser();
+
+        if (! $user) {
+            throw new LogicException('An unknown error occurred');
+        }
+
+        $response = $this->response->withHeader('Content-Type', 'text/html');
+
+        if ($user->getExtendedProperty('is_admin') !== 1) {
+            $response->getBody()->write(
+                $this->twigEnvironment->renderAndMinify(
+                    'account/Unauthorized.twig'
+                )
+            );
+
+            return $response;
+        }
+
+        $userSelectArray = $this->additionalUserActions->fetchAsSelectArray();
+
+        $response->getBody()->write(
+            $this->twigEnvironment->renderAndMinify('StandardPage.twig', [
+                'metaTitle' => 'Create Ticket',
+                'breadCrumbs' => [
+                    [
+                        'href' => '/tickets',
+                        'content' => 'Tickets',
+                    ],
+                    ['content' => 'Create Ticket'],
+                ],
+                'title' => 'Create Ticket',
+                'includes' => [
+                    [
+                        'template' => 'forms/StandardForm.twig',
+                        'actionParam' => 'createTicket',
+                        'inputs' => [
+                            [
+                                'template' => 'Select',
+                                'name' => 'assigned_to_user_guid',
+                                'label' => 'Assigned To',
+                                'options' => $userSelectArray,
+                            ],
+                            [
+                                'template' => 'Text',
+                                'type' => 'text',
+                                'name' => 'title',
+                                'label' => 'Title',
+                            ],
+                            [
+                                'template' => 'TextArea',
+                                'name' => 'content',
+                                'label' => 'Content',
+                            ],
+                            [
+                                'template' => 'Select',
+                                'name' => 'watchers[]',
+                                'label' => 'Additional Ticket Watchers (assignee and reporter always receive notifications)',
+                                'options' => $userSelectArray,
+                                'multiple' => true,
+                            ],
+                        ],
+                    ],
+                ],
+            ])
+        );
+
+        return $response;
     }
 }
