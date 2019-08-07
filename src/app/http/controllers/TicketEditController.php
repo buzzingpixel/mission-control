1c21e193fa8389246dfc95e1ad26de72a95d4d98
TJ Draper <tj@buzzingpixel.com>
2019-08-06 22:42:32 -0500

    Add a route for editing a ticket

---
+++ src/app/http/controllers/TicketEditController.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace src\app\http\controllers;
+
+class TicketEditController
+{
+    public function __invoke() : void
+    {
+        // TODO: Implement method
+        dd('TODO: Implement ' . __METHOD__ . '()');
+    }
+}
