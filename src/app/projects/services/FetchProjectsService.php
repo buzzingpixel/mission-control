8af0329b4d84f554f29e4b3715d9e84fed63356c
TJ Draper <tj@buzzingpixel.com>
2019-08-31 11:16:54 -0500

    Update fetch projects service to hydrate key/value items

--- src/app/projects/services/FetchProjectsService.php
+++ src/app/projects/services/FetchProjectsService.php
@@ -12,6 +12,9 @@ use src\app\data\Project\Project;
 use src\app\data\Project\ProjectRecord;
 use src\app\projects\interfaces\ProjectModelInterface;
 use src\app\projects\models\ProjectModel;
+use function is_array;
+use function is_string;
+use function json_decode;
 
 class FetchProjectsService
 {
@@ -42,11 +45,24 @@ class FetchProjectsService
         foreach ($this->fetchResults($params) as $record) {
             $model = new ProjectModel();
 
+            $keyValueItems = $record->key_value_items;
+
+            if (! is_string($keyValueItems)) {
+                $keyValueItems = '';
+            }
+
+            $keyValueItems = json_decode($keyValueItems, true);
+
+            if (! is_array($keyValueItems)) {
+                $keyValueItems = [];
+            }
+
             $model->setGuidAsBytes($record->guid);
             $model->isActive($record->is_active === 1 || $record->is_active === '1');
             $model->title($record->title);
             $model->slug($record->slug);
             $model->description($record->description);
+            $model->keyValueItems($keyValueItems);
             /** @noinspection PhpUnhandledExceptionInspection */
             $model->addedAt(new DateTime(
                 $record->added_at,
